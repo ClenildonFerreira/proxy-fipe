c73d72168a9ea36692ae98058681c84784d43a9d
Clenildon Ferreira <clenildonferreira34@gmail.com>
2024-11-15 16:30:15 -0300

    teste porta

--- proxy.js
+++ proxy.js
@@ -46,7 +46,7 @@ app.get("/health", (req, res) => {
 });
 
 // Configuração da porta dinâmica
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
