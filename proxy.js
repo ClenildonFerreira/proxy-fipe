fabde13ec46cb8d4c1048cd6eaf41ed42944dd59
Clenildon Ferreira <clenildonferreira34@gmail.com>
2024-11-15 15:52:05 -0300

    test proxy

--- proxy.js
+++ proxy.js
@@ -12,15 +12,18 @@ app.post("/proxy", async (req, res) => {
 
     // Validações básicas
     if (!placa || !token) {
+        console.error("Erro: Parâmetros inválidos.", req.body);
         return res.status(400).json({ error: "Placa e token são obrigatórios!" });
     }
 
     try {
         // Fazendo a requisição para a API externa
+        console.log("Requisição para a API PlacaFipe:", { placa, token });
         const response = await axios.post("https://api.placafipe.com.br/getplacafipe", { placa, token });
         res.json(response.data);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        console.error("Erro ao chamar a API externa:", error.message);
+        res.status(500).json({ error: "Erro ao processar a requisição" });
     }
 });
 
